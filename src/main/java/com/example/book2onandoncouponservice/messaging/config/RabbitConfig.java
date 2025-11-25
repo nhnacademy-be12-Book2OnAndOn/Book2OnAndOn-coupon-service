11ca96bc8f5373e4b9728d96c89bc9ea4ecdd758
OiJs <oijs9663@gmail.com>
2025-11-25 10:47:42 +0900

    Feature: deploy

---
+++ src/main/java/com/example/book2onandoncouponservice/messaging/config/RabbitConfig.java
@@ -0,0 +1,21 @@
+//package com.example.book2onandoncouponservice.messaging.config;
+//
+//import org.springframework.amqp.core.DirectExchange;
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.context.annotation.Bean;
+//
+//public class RabbitConfig {
+//
+//    public static final String EXCHANGE = "user-exchange";
+//    public static final String QUEUE = "user-signup-coupon-queue";
+//    public static final String ROUTING_KEY = "user.signup";
+//
+//    @Bean
+//    public Queue queue() {
+//       return new Queue(QUEUE, true);
+//    }
+//
+//    @Bean
+//    public DirectExchange
+//
+//}
