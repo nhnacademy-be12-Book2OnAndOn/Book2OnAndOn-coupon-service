4ab5c17c6e9c4005470182d96163faec9cde42e6
OiJs <oijs9663@gmail.com>
2025-11-26 16:57:56 +0900

    Feat: @EnableScheduling 추가

--- src/main/java/com/example/book2onandoncouponservice/Book2onandoncouponserviceApplication.java
+++ src/main/java/com/example/book2onandoncouponservice/Book2onandoncouponserviceApplication.java
@@ -3,10 +3,12 @@ package com.example.book2onandoncouponservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class Book2onandoncouponserviceApplication {
 
     public static void main(String[] args) {
